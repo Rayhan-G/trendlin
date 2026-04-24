99a61eb9be0c02d306821f3d4fb19fc2b4865a15
Rayhan-G <rayhangazi21111@gmail.com>
2026-04-25 00:10:11 +0600

    Configure @ path alias for Vercel

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   
-  // Configure module aliases
+  // Configure path aliases
   webpack: (config, { isServer }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': __dirname,
+      '@': path.resolve(__dirname, './src'),
     }
     return config
   },
   
-  // Environment variables that will be available
+  // Environment variables
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
