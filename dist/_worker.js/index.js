76541c1dcd5c8ee4ad89909afbf47f5867a0158a
Rayhan-G <rayhangazi21111@gmail.com>
2026-07-11 19:05:28 +0600

    Update social media links in footer

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { createExports } from './_@astrojs-ssr-adapter.mjs';
-import { manifest } from './manifest_C7pOtjtM.mjs';
+import { manifest } from './manifest_CGjmjPpF.mjs';
 
 const serverIslandMap = new Map();;
 
