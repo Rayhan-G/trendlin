aba52a48401c282c7241b8c9723ff55b8f542702
Rayhan-G <rayhangazi21111@gmail.com>
2026-07-14 14:47:07 +0600

    admin version-16

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { createExports } from './_@astrojs-ssr-adapter.mjs';
-import { manifest } from './manifest_DQ91a7ro.mjs';
+import { manifest } from './manifest_C6bJZb7F.mjs';
 
 const serverIslandMap = new Map();;
 
