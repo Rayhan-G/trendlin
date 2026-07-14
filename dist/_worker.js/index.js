3213321cc8443daed29dac5254ed31491a3954f6
Rayhan-G <rayhangazi21111@gmail.com>
2026-07-14 15:03:28 +0600

    admin version-16

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { createExports } from './_@astrojs-ssr-adapter.mjs';
-import { manifest } from './manifest_DIneEErV.mjs';
+import { manifest } from './manifest_HT-g5kS4.mjs';
 
 const serverIslandMap = new Map();;
 
