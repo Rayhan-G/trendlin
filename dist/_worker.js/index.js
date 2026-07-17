f35f96a06978e742f0518fe9d5c60ab7f0196315
Rayhan-G <rayhangazi21111@gmail.com>
2026-07-17 13:49:00 +0600

    dashboard update

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { createExports } from './_@astrojs-ssr-adapter.mjs';
-import { manifest } from './manifest_kJSiopml.mjs';
+import { manifest } from './manifest_CKnAeaWg.mjs';
 
 const serverIslandMap = new Map();;
 
